88d0ad16b3d135f65351593e0270cb11378a6757
HansanaWeerakkody <hansanaweerakkody70@gmail.com>
2025-12-07 22:03:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchPage from './pages/SearchPage';
+import PropertyPage from './pages/PropertyPage';
 import './App.css';
 
 function App() {
+  // Global State for Favourites (Lifted State)
+  const [favourites, setFavourites] = useState([]);
+
+  // Function to add item (prevents duplicates)
+  const addToFavourites = (property) => {
+    if (!favourites.some(fav => fav.id === property.id)) {
+      setFavourites([...favourites, property]);
+    } else {
+      alert("Property already in favourites!");
+    }
+  };
+
+  // Function to remove item
+  const removeFromFavourites = (id) => {
+    setFavourites(favourites.filter(prop => prop.id !== id));
+  };
+
+  // Function to clear all
+  const clearFavourites = () => {
+    setFavourites([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="app-header">
+          <h1>Estate Agent App</h1>
+        </header>
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <SearchPage 
+                favourites={favourites} 
+                addFav={addToFavourites} 
+                removeFav={removeFromFavourites} 
+                clearFav={clearFavourites}
+              />
+            } 
+          />
+          <Route 
+            path="/property/:id" 
+            element={<PropertyPage addFav={addToFavourites} />} 
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
